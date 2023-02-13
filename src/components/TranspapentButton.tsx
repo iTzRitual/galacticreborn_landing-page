37c191da81910c0c9730a3fe5758ef835d0729bc
Natan Mokrzycki <ritualhorse960@gmail.com>
2023-02-13 02:05:16 +0100

    TransparentButton component

---
+++ src/components/TranspapentButton.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button, Typography } from "@mui/material";
+
+interface TransparentButtonProps {
+  children: React.ReactNode;
+}
+
+function StickyButton({ children }: TransparentButtonProps) {
+  return (
+    <div
+      style={{
+        padding: "10px 35px",
+        backgroundColor: "white",
+        color: "black",
+        mixBlendMode: "screen",
+        fontSize: "16px",
+        borderRadius: "15px",
+        fontFamily: "Inter",
+        fontWeight: "700",
+        textAlign: "center",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default StickyButton;
